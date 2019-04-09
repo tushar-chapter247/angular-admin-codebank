abbc51bfff138b3250497f6e3850e64f514f9d56
Tushar Goyal <tushar.chapter247@gmail.com>
2019-04-09 12:49:15 +0530

    add button text for snackbar

--- src/app/core/services/notification/notification.service.ts
+++ src/app/core/services/notification/notification.service.ts
@@ -8,8 +8,8 @@ export class NotificationService {
   constructor(private snackbar: MatSnackBar) {}
 
   // NOTIFY USER BY SHOWING MESSAGE IN SNACKBAR
-  async notify(message: string): Promise<any> {
-    this.snackbar.open(message, null, {
+  async notify(message: string, buttonText?: string): Promise<any> {
+    this.snackbar.open(message, buttonText ? buttonText : null, {
       duration: 4000,
       horizontalPosition: 'right',
       verticalPosition: 'bottom',
